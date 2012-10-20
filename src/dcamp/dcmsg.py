072a9c7291e99f47907ac39e6edbc96459e8e460
Alexander Sideropoulos <alexander@thequery.net>
2012-10-19 20:26:55 -0400

    Update dcmsg module to work with bytes instead of unicode. Add logging to dcmsg module.

--- src/dcamp/dcmsg.py
+++ src/dcamp/dcmsg.py
@@ -1,18 +1,28 @@
 '''
 dCAMP message module
 '''
+import logging
 
 class DCMsg(object):
 	'''
 	Base dCAMP message
 	'''
 
-	def __init__(self, frames=None):
-		self._frames = list() if frames is None else frames
+	logger = logging.getLogger("dcamp.dcmsg")
+
+	@property
+	def frames(self):
+		return list();
+
+	@property
+	def name(self):
+		return self.__class__.__name__.encode()
 
 	def send(self, socket):
-		assert isinstance(self._frames, list)
-		socket.send_multipart(self._frames)
+		socket.send_multipart(self.frames)
+
+	def __iter__(self):
+		return iter(self.frames)
 
 	@classmethod
 	def recv(cls, socket):
@@ -26,42 +36,45 @@ class DCMsg(object):
 
 		key = msg[0]
 		for c in cls.__subclasses__():
-			if c.__name__ == key:
+			if c.__name__.encode() == key:
+				cls.logger.debug("found subclass match: %s", c.__name__)
 				return c.from_msg(msg) # class found, so return it
 
-		return cls(msg) # if class not found, return generic
+		cls.logger.debug("no subclass matches found")
+		return cls() # if class not found, return generic
 
 class MARCO(DCMsg):
 	def __init__(self, root_endpoint):
-		assert isinstance(root_endpoint, str)
+		assert isinstance(root_endpoint, bytes)
+		self.root_endpoint = root_endpoint
 
 	@property
 	def frames(self):
-		return [self.__class__.__name__, root_endpoint]
+		return [self.name, self.root_endpoint]
 
 	@classmethod
 	def from_msg(cls, msg):
 		# assert we have at two frames and correct key
 		assert isinstance(msg, list)
 		assert 2 == len(msg)
-		assert cls.__name__ == msg[0]
+		assert cls.__name__.encode() == msg[0]
 		return cls(msg[1])
 
 class POLO(DCMsg):
 	def __init__(self, base_endpoint):
-		assert isinstance(base_endpoint, str)
+		assert isinstance(base_endpoint, bytes)
 		self.base_endpoint = base_endpoint
 
 	@property
 	def frames(self):
-		return [self.__class__.__name__, base_endpoint]
+		return [self.name, self.base_endpoint]
 
 	@classmethod
 	def from_msg(cls, msg):
 		# assert we have at two frames and correct key
 		assert isinstance(msg, list)
 		assert 2 == len(msg)
-		assert cls.__name__ == msg[0]
+		assert cls.__name__.encode() == msg[0]
 		return cls(msg[1])
 
 class ASSIGN(DCMsg):
@@ -69,7 +82,7 @@ class ASSIGN(DCMsg):
 	import zmq.utils.jsonapi as json
 
 	def __init__(self, parent_endpoint, properties=None):
-		assert isinstance(parent_endpoint, str)
+		assert isinstance(parent_endpoint, bytes)
 		assert properties is None or isinstance(properties, dict)
 		self.parent_endpoint = parent_endpoint
 		self.properties = dict() if properties is None else properties
@@ -86,8 +99,8 @@ class ASSIGN(DCMsg):
 
 	@property
 	def frames(self):
-		return [self.__class__.__name__,
-				parent_endpoint,
+		return [self.name,
+				self.parent_endpoint,
 				json.dumps(self.properties)]
 
 	@classmethod
@@ -95,25 +108,25 @@ class ASSIGN(DCMsg):
 		# assert we have at three frames and correct key
 		assert isinstance(msg, list)
 		assert 3 == len(msg)
-		assert cls.__name__ == msg[0]
+		assert cls.__name__.encode() == msg[0]
 
 		return cls(msg[1], properties=json.loads(msg[2]))
 
 class WTF(DCMsg):
 	def __init__(self, errcode, errstr=None):
 		assert isinstance(errcode, int)
-		assert errstr is None or isinstance(errstr, str)
+		assert errstr is None or isinstance(errstr, bytes)
 		self.errcode = errcode
 		self.errstr = '' if errstr is None else errstr
 
 	@property
 	def frames(self):
-		return [self.__class__.__name__, errcode, errstr]
+		return [self.name, errcode, errstr]
 
 	@classmethod
 	def from_msg(cls, msg):
 		# assert we have at two frames and correct key
 		assert isinstance(msg, list)
 		assert 2 == len(msg)
-		assert cls.__name__ == msg[0]
+		assert cls.__name__.encode() == msg[0]
 		return cls(msg[1])
