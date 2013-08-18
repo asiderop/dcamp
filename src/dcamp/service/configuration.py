74280584a95312027181de0b7e0128b959b86c5b
Alexander Sideropoulos <alexander@thequery.net>
2013-08-17 17:16:59 -0700

    Barebones kvsync request and reply / refs #30
    - Fixes issue with POLLIN not being set during poller registration
    - Simplifies kvsync request by combining all topics into single request

--- src/dcamp/service/configuration.py
+++ src/dcamp/service/configuration.py
@@ -1,5 +1,5 @@
 import logging, zmq, random
-from time import time
+from time import time, sleep
 
 import dcamp.types.messages.configuration as ConfigMsg
 from dcamp.types.specs import EndpntSpec
@@ -66,35 +66,49 @@ class Configuration(Service):
 
 			self.poller.register(self.update_sub, zmq.POLLIN)
 
-			# 2) request snapshot from parent
-			#self.kvsync_req = self.ctx.socket(zmq.DEALER)
-			#icanhaz = ConfigMsg.ICANHAZ()
-			#icanhaz.send(self.kvsync_req, prefix=[b'']) # DEALER socket needs empty first frame
-			self.state = Configuration.STATE_SYNC
+			# 2) request snapshot(s) from parent
+			self.kvsync_req = self.ctx.socket(zmq.DEALER)
+			self.kvsync_req.connect(self.parent.connect_uri(EndpntSpec.CONFIG_SNAPSHOT))
+			icanhaz = ConfigMsg.ICANHAZ(';'.join(topics))
+			icanhaz.send(self.kvsync_req)
 
-			# XXX: just for testing
-			self.__setup_outbound()
-			#self.poller.register(self.kvsync_req)
+			self.poller.register(self.kvsync_req, zmq.POLLIN)
+
+			self.state = Configuration.STATE_SYNC
 
 		else:
 			assert 'root' == self.level
+
+			# XXX: just for testing
+			self.kvdict['/config/group1/endpoints'] = ("localhost:5600, siderop1-losx.local:5630, localhost:5660", 0)
+			self.kvdict['/config/group1/filters'] = ("None", 1)
+			self.kvdict['/config/group1/metrics'] = ("CPU(rate='5s', threshold='30s')", 2)
+			self.kvdict['/config/group2/endpoints'] = ("localhost:5700, localhost:5730, localhost:5760", 3)
+			self.kvdict['/config/group2/filters'] = ("-'192.168.1.0/24', +'.netapp.com'", 4)
+			self.kvdict['/config/group2/metrics'] = ("CPU(rate='5s', threshold='30s'), DISK(rate='10s', threshold='>5000')", 5)
+			self.kvdict['/config/group3/endpoints'] = ("192.168.1.102:5800, 192.168.1.102:5830, 192.168.1.102:5860", 6)
+			self.kvdict['/config/group3/filters'] = ("None", 7)
+			self.kvdict['/config/group3/metrics'] = ("NETWORK(rate='60s', threshold='None')", 8)
+			self.kvdict['/config/root/endpoint'] = ("localhost:5500", 9)
+			self.kvdict['/config/root/heartbeat'] = ("5", 10)
+
 			self.pubnext = time() + 1
 			self.__setup_outbound()
 
 	def __setup_outbound(self):
 		if self.level in ['branch', 'root']:
 			# 3) publish updates to children (bind)
-			# XXX: only publish config updates to non-collector children
 			self.update_pub = self.ctx.socket(zmq.PUB)
 			self.update_pub.bind(self.endpoint.bind_uri(EndpntSpec.CONFIG_UPDATE))
 
 			# 4) service snapshot requests to children (bind)
 			self.kvsync_rep = self.ctx.socket(zmq.ROUTER)
 			self.kvsync_rep.bind(self.endpoint.bind_uri(EndpntSpec.CONFIG_SNAPSHOT))
+			self.poller.register(self.kvsync_rep, zmq.POLLIN)
 
-			# XXX: process pending updates
-			# for update in self.pending_updates:
-			#	__process_update_message()
+			# process pending updates
+			for update in self.pending_updates:
+				self.__process_update_message(update)
 
 			self.state = Configuration.STATE_GOGO
 
@@ -102,6 +116,11 @@ class Configuration(Service):
 		# service exiting; return some status info and cleanup
 		self.logger.debug("%d subs; %d pubs; %d reqs; %d reps" %
 				(self.subcnt, self.pubcnt, self.reqcnt, self.repcnt))
+
+		# print each key-value pair; value is really (value, seq-num)
+		sleep(1)
+		for (k, (v, s)) in self.kvdict.items():
+			self.logger.debug('%s: %s (%d)' % (k, v, s))
 
 		if self.update_sub is not None:
 			self.update_sub.close()
@@ -115,14 +134,14 @@ class Configuration(Service):
 		super()._cleanup()
 
 	def _pre_poll(self):
-		# send fake updates to random groups, just for testing
+		# XXX: send fake updates to random groups, just for testing
 		if self.level == 'root':
 			if self.pubnext < time():
 				self.kv_seq += 1
-				pubmsg = ConfigMsg.KVPUB('/config/group%d/test-key' % random.randint(1,3),
-						'test-val', self.kv_seq - random.randint(0, 2)) # randomize the seq number
+				pubmsg = ConfigMsg.KVPUB('/config/group%d/test-key-%d' % (random.randint(1,3), random.randint(0,30)),
+						'test-val', self.kv_seq + random.randint(0, 0)) # randomize the seq number
 				pubmsg.send(self.update_pub)
-				self.pubnext = time() + 5
+				self.pubnext = time() + 1
 				self.pubcnt += 1
 
 			self.poller_timer = 1e3 * max(0, self.pubnext - time())
@@ -135,15 +154,6 @@ class Configuration(Service):
 		if self.kvsync_rep in items:
 			self.__send_snapshot()
 
-	def __recv_snapshot(self, child):
-		assert self.state == Configuration.STATE_SYNC
-		# XXX: if KTHXBYE, __setup_outbound()
-		pass
-
-	def __send_snapshot(self, child):
-		assert self.state == Configuration.STATE_GOGO
-		pass
-
 	def __recv_update(self):
 		update = ConfigMsg.CONFIG.recv(self.update_sub)
 		self.subcnt += 1
@@ -153,24 +163,71 @@ class Configuration(Service):
 			return
 
 		if Configuration.STATE_SYNC == self.state:
+			# TODO: could just not read the message; let them queue up on the socket
+			#       itself...
 			self.pending_updates.append(update)
 		elif Configuration.STATE_GOGO == self.state:
 			self.__process_update_message(update)
 		else:
 			raise NotImplementedError('unknown state')
 
-	def __process_update_message(self, update):
+	def __process_update_message(self, update, allow_out_of_sequence=False):
 		# if not greater than current kv-sequence, skip this one
-		if update.sequence <= self.kv_seq:
+		if not allow_out_of_sequence and update.sequence <= self.kv_seq:
 			self.logger.warn('KVPUB out of sequence (cur=%d, recvd=%d); dropping' % (
 					self.kv_seq, update.sequence))
 			return
 
-		self.kv_seq = update.sequence
+		if allow_out_of_sequence:
+			# during sync, we allow out of sequence updates; we only set our seq-num when
+			# we get a message that is higher than any previous message
+			if update.sequence > self.kv_seq:
+				self.kv_seq = update.sequence
+		else:
+			self.kv_seq = update.sequence
+
 		self.kvdict[update.key] = (update.value, update.sequence)
 		if len(update.value) == 0:
 			del self.kvdict[update.key]
 
+		# this should be None if still in SYNC state
 		if self.update_pub is not None:
 			update.send(self.update_pub)
 			self.pubcnt += 1
+
+	def __recv_snapshot(self):
+		assert Configuration.STATE_SYNC == self.state
+
+		# should either be KVSYNC or KTHXBAI
+		response = ConfigMsg.CONFIG.recv(self.kvsync_req)
+
+		if response.is_error:
+			self.logger.error(response)
+			return
+
+		if ConfigMsg.CONFIG.KTHXBAI == response.ctype:
+			# TODO: if response.sequence != self.kv_seq: wait? request new snap?
+			if response.sequence != self.kv_seq:
+				self.logger.warn('final seq-num does not match (cur=%d, recvd=%d)'
+						% (self.kv_seq, response.sequence))
+			self.__setup_outbound()
+		else:
+			self.__process_update_message(response, allow_out_of_sequence=True)
+
+	def __send_snapshot(self):
+		assert Configuration.STATE_GOGO == self.state
+
+		request = ConfigMsg.CONFIG.recv(self.kvsync_rep)
+
+		if request.is_error:
+			self.logger.error(request)
+			return
+
+		peer_id = request._peer_id
+		subtrees = request.value.split(';') # subtree stored as value in ICANHAZ message
+
+		# XXX: for (k, (v, s)) in kvdict.items():
+		#    send KVSYNC
+
+		snap = ConfigMsg.KTHXBAI(self.kv_seq, peer_id, ';'.join(subtrees))
+		snap.send(self.kvsync_rep)
