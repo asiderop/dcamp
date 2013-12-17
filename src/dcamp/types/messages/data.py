cb8b1c8c4e785d39155b5956de69f8e4a1154432
Alexander Sideropoulos <alexander@thequery.net>
2013-12-16 22:20:02 -0800

    making DATA class more sane with print() and calculate()

--- src/dcamp/types/messages/data.py
+++ src/dcamp/types/messages/data.py
@@ -10,10 +10,9 @@ from dcamp.util.functions import format_bytes, isInstance_orNone, now_msecs
 __all__ = [
 	'DATA_HUGZ',
 	'DATA_BASIC',
-	'DATA_SUM',
+	'DATA_RATE',
 	'DATA_AVERAGE',
 	'DATA_PERCENT',
-	'DATA_RATE',
 	'_DATA',
 	]
 
@@ -21,23 +20,17 @@ class _DATA(DCMsg, _PROPS):
 	'''
 	Frame 0: data source (leaf or collector node endpoint), as 0MQ string
 	Frame 1: properties, as 0MQ string
-	Frame 2: time t1 in ms epoch utc, 8 bytes in network order
-	Frame 3: value v1, 8 bytes in network order
-	Frame 4: time t2 in ms epoch utc, 8 bytes in network order; not empty for average and rate
-	Frame 5: value v2, 8 bytes in network order; empty for basic and sum
+	Frame 2: time in ms epoch utc, 8 bytes in network order
+	Frame 3: value, 8 bytes in network order
+	Frame 4: base value, 8 bytes in network order; only for average and percent types
 
 	properties = *( type / detail / config )
-	type       = "type=" ( "HUGZ" / "basic" / "sum" / "average" / "percent" / "rate" )
+	type       = "type=" ( "HUGZ" / "basic" / "rate" / "average" / "percent" )
 	detail     = "detail=" <string>
 	config     = "config-name=" <string>
-
-	_DATA.time represents the time at which this data message represents. If
-	this data message represents a time range, _DATA.time is the end time and
-	_DATA.base_time is the start time.
 	'''
 
-	def __init__(self, source, properties,
-			time1=None, value1=None, value2=None):
+	def __init__(self, source, properties, time=None, value=None, base_value=None):
 		DCMsg.__init__(self)
 		_PROPS.__init__(self, properties)
 
@@ -50,17 +43,17 @@ class _DATA(DCMsg, _PROPS):
 		# validate class was constructed with type-appropriate sub-class
 		assert isinstance(self, _MTYPES[self.m_type])
 
-		assert isInstance_orNone(time1, int)
-		assert isInstance_orNone(value1, int)
-		assert isInstance_orNone(value2, int)
+		assert isInstance_orNone(time, int)
+		assert isInstance_orNone(value, int)
+		assert isInstance_orNone(base_value, int)
 
 		# TODO: add more verifications of parameters based on given m_type
 
 		self.source = source
 
-		self.time1 = time1
-		self.value1 = value1
-		self.value2 = value2
+		self.time = time
+		self.value = value
+		self.base_value = base_value
 
 	@property
 	def m_type(self):
@@ -72,29 +65,55 @@ class _DATA(DCMsg, _PROPS):
 	def config_name(self):
 		return self.get('config-name', None)
 
-	@property
-	def calculated(self):
-		'''returns float representing calculated value of data message'''
+	def __verify_compatibility(self, given):
+		assert self.source == given.source, 'different source; maybe invalid'
+		assert self.m_type == given.m_type, 'different message type'
+		assert self.time < given.time, 'wrong sample order'
+
+	def print(self, given):
+		'''
+		Return value is a string representation of the calculated value and a contextual
+		suffix.
+		'''
+		return '%.2f %s' % (self.calculate(given), self.suffix(given))
+
+	def calculate(self, given):
+		'''
+		Calculates a value based on this and the given data message. The given message
+		must be from the same source, have the same message type, and have a later
+		timestamp. Return value is a float representation of calculated result.
+		'''
+		self.__verify_compatibility(given)
+		return self._calculate(given)
+
+	def suffix(self, given):
+		'''
+		Calculates a value based on this and the given data message. The given message
+		must be from the same source, have the same message type, and have a later
+		timestamp. Return value is a float representation of calculated result.
+		'''
+		self.__verify_compatibility(given)
+		return self._suffix(given)
+
+	def _calculate(self, given):
+		raise NotImplementedError('sub-class implementation missing')
+	def _suffix(self):
 		raise NotImplementedError('sub-class implementation missing')
 
-	@property
-	def suffix(self):
-		return ''
-
+	# XXX: cleanup these two methods
 	def __str__(self):
-		return '%s -- %s @ %d = %.2f' % (self.source, self.detail, self.time1, self.calculated)
-
+		return '%s -- %s @ %d = %.2f' % (self.source, self.detail, self.time, self._calculate)
 	def log_str(self):
-		return '%d\t%s\t%s\t%.2f%s' % (self.time1, self.source, self.detail, self.calculated, self.suffix)
+		return '%d\t%s\t%s\t%.2f%s' % (self.time, self.source, self.detail, self._calculate, self.suffix)
 
 	@property
 	def frames(self):
 		return [
 				self.source.encode(),
 				self._encode_dict(self.properties),
-				self._encode_int(self.time1),
-				self._encode_int(self.value1),
-				self._encode_int(self.value2),
+				self._encode_int(self.time),
+				self._encode_int(self.value),
+				self._encode_int(self.base_value),
 			]
 
 	@classmethod
@@ -108,74 +127,67 @@ class _DATA(DCMsg, _PROPS):
 		source = EndpntSpec.decode(msg[0])
 		props = _PROPS._decode_dict(msg[1])
 
-		time1 = DCMsg._decode_int(msg[2])
-		value1 = DCMsg._decode_int(msg[3])
-		value2 = DCMsg._decode_int(msg[4])
+		time = DCMsg._decode_int(msg[2])
+		value = DCMsg._decode_int(msg[3])
+		base_value = DCMsg._decode_int(msg[4])
 
-		return cls(source, props, time1, value1, value2)
+		return cls(source, props, time, value, base_value)
 
 def HUGZ(endpoint):
 	return DATA_HUGZ(
 			source=endpoint,
 			properties={'type': 'HUGZ'},
-			time1=now_msecs()
+			time=now_msecs()
 		)
 
 class DATA_HUGZ(_DATA):
+	def _calculate(self, given):
+		raise NotImplementedError('HUGZ have no value')
+
+	def _suffix(self, given):
+		raise NotImplementedError('HUGZ have no suffix')
+
 	def __str__(self):
-		return '%s -- HUGZ @ %d' % (str(self.source), self.time1)
+		return '%s -- HUGZ @ %d' % (str(self.source), self.time)
 
 	def log_str(self):
-		return '%d\t%s\t%s' % (self.time1, self.source, self.m_type)
+		return '%d\t%s\t%s' % (self.time, self.source, self.m_type)
 
 class DATA_BASIC(_DATA):
-	@property
-	def calculated(self):
-		return float(self.value1)
+	def _calculate(self, given):
+		return float(given.value - self.value)
 
-class DATA_SUM(_DATA):
-	@property
-	def suffix(self):
-		return ' total'
-
-	@property
-	def calculated(self):
-		return float(self.value1)
+	def _suffix(self, given):
+		return ''
 
 class DATA_AVERAGE(_DATA):
-	@property
-	def suffix(self):
-		return ' average'
+	def _suffix(self, given):
+		return 'average'
 
-	@property
-	def calculated(self):
-		return (self.value1 / self.value2)
+	def _calculate(self, given):
+		return float( (given.value - self.value) / (given.base_value - self.base_value) )
 
 class DATA_PERCENT(_DATA):
-	@property
-	def suffix(self):
+	def _suffix(self, given):
 		return '%'
 
-	@property
-	def calculated(self):
-		return (self.value1 / self.value2) * 100
+	def _calculate(self, given):
+		return float( (given.value - self.value) / (given.base_value - self.base_value) ) * 100.0
 
 class DATA_RATE(_DATA):
-	@property
-	def suffix(self):
-		#return '%s / sec' % format_bytes((self.value2 - self.value1) / (self.time2 - self.time1) * 1e3, num_or_suffix='suffix')
-		pass
+	def __the_rate(self, given):
+		return float( (given.value - self.value) / (given.time - self.time) * 1e3 )
 
-	@property
-	def calculated(self):
-		#return format_bytes((self.value2 - self.value1) / (self.time2 - self.time1) * 1e3, num_or_suffix='num')
-		pass
+	def _suffix(self, given):
+		return '%s / sec' % format_bytes(self.__the_rate(given), num_or_suffix='suffix')
+
+	def _calculate(self, given):
+		return format_bytes(self.__the_rate(given), num_or_suffix='num')
 
 _MTYPES = {
 	'HUGZ': DATA_HUGZ,
 	'basic': DATA_BASIC,
-	'sum': DATA_SUM,
+	'rate': DATA_RATE,
 	'average': DATA_AVERAGE,
 	'percent': DATA_PERCENT,
-	'rate': DATA_RATE,
 }
